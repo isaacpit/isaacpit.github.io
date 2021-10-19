0832c490d57249f5e7e75be52073647f9766db7e
isaacpitblado <isaacpitblado@tamu.edu>
2021-10-18 21:22:59 -0500

    changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
