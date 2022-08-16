999808af203b5c9f5c566237cb88e5b6e973472f
isaacpitblado <isaacpitblado@tamu.edu>
2022-08-16 02:28:49 -0500

    updated nav w/ band logo

--- src/components/NavbarAlt.jsx
+++ src/components/NavbarAlt.jsx
@@ -6,18 +6,20 @@ class NavbarAlt extends Component {
   render() {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <img className="band-logo" alt="mr-kat-band-logo" src={require("./../images/lifePics/mr-kat-logo.jpg")} />
       <a className="navbar-brand" href="#">Isaac Pitblado</a>
+      
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          {/* <li className="nav-item active">
-            <a className="nav-link" href="#Bio">Bio <span className="sr-only">(current)</span></a>
-          </li> */}
+          {/* {/* <li className="nav-item active">
+            <a className="nav-link" href="#Bio">Bio <span className="sr-only">(current)</span></a> */}
+          {/* </li> */}
           {/* <li className="nav-item">
-            <a className="nav-link" href="#SimpleTimeline">Experience</a>
+            <a className="nav-link" href="titleExperience">Experience</a>
           </li> */}
           {/* DON'T DELETE */}
           {/* <li className="nav-item dropdown">
