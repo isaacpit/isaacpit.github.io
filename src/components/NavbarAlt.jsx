90c8d53c03ef72d512446668d537741b45645855
isaacpitblado <isaacpitblado@tamu.edu>
2022-08-17 01:27:00 -0500

    added nav links which work w/o needing BrowserRouter (gh-pages limitation). They use scrollIntoView + window.history.pushState

--- src/components/NavbarAlt.jsx
+++ src/components/NavbarAlt.jsx
@@ -1,53 +1,86 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Bio from './Bio';
 import './NavbarAlt.css';
 
-class NavbarAlt extends Component {
-  render() {
-    return (
+const InPageAnchors = ({text, searchId, statePushUrl}) => {
+
+
+  return (
+    <a className="navbar-link"
+        href="/"
+        onClick={(e) => {
+          let hero = document.getElementById(searchId);
+          e.preventDefault();
+          hero && hero.scrollIntoView({ behavior: "smooth", block: "start" });
+          // hero.scrollIntoView({ block: "start" });
+          window.history.pushState("TEST", "TEST", statePushUrl);
+        }}
+      >
+      {text}
+    </a>
+  );
+  
+}
+const NavbarAlt = () => {
+
+  const location = useLocation().pathname;
+  const IS_HOME = location == "/";
+  console.log("LOCATION=", location);
+  console.log("IS_HOME=", IS_HOME);
+
+
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <img className="band-logo" alt="mr-kat-band-logo" src={require("./../images/lifePics/mr-kat-logo.jpg")} />
-      <a className="navbar-brand" href="#">Isaac Pitblado</a>
+      <a href="/">
+        <img id="navbar-brand-logo" alt="mr-kat-band-logo" src={require("./../images/lifePics/mr-kat-logo.jpg")} /> 
+      </a>
+      
+      <a id="navbar-brand-name" className="navbar-brand" href="/">Isaac's Portfolio</a>
       
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          {/* {/* <li className="nav-item active">
-            <a className="nav-link" href="#Bio">Bio <span className="sr-only">(current)</span></a> */}
-          {/* </li> */}
-          {/* <li className="nav-item">
-            <a className="nav-link" href="titleExperience">Experience</a>
-          </li> */}
-          {/* DON'T DELETE */}
-          {/* <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li> */}
-          {/* <li className="nav-item">
-            <a className="nav-link" href="#Portfolio">Portfolio</a>
-          </li> */}
-          {/* <li className="nav-item">
-            <a className="nav-link" href="#Focus">Focuses</a>
-          </li> */}
+
+      
+        <ul className="navbar-nav align-items-center justify-content-center">
+
+          
+
+          {IS_HOME &&
+            <>
+              <InPageAnchors 
+                text="About Me"
+                searchId="about-me-header"
+                statePushUrl="/"
+              />
+              <InPageAnchors 
+                text="Experience"
+                searchId="experience-header"
+                statePushUrl="/"
+              />
+              <InPageAnchors 
+                text="Portfolio"
+                searchId="portfolio-header"
+                statePushUrl="/"
+              />
+              <InPageAnchors 
+                text="After Hours"
+                searchId="for-fun-header"
+                statePushUrl="/"
+              />
+            
+            </>
+          }   
+        
+          <Link id="link-to-band-page" to="/mr-kat">Band Page</Link>
+
         </ul>
-        {/* <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form> */}
       </div>
     </nav>
-    );
-  }
+  );
 }
 
 export default NavbarAlt;
